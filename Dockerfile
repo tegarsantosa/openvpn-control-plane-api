e1fa5d77fae99276464323ae80e7a96a2f676abb
Tegar Santosa <arsan.personal@gmail.com>
2026-01-11 18:55:37 +0700

    fix: deployment

--- Dockerfile
+++ Dockerfile
@@ -38,7 +38,7 @@ USER nodejs
 EXPOSE 3000
 
 HEALTHCHECK --interval=30s --timeout=10s --start-period=5s --retries=3 \
-    CMD node -e "require('http').get('http://localhost:3000/health', (r) => {process.exit(r.statusCode === 200 ? 0 : 1)})"
+    CMD node -e "require('http').get('http://0.0.0.0:3000/health', (r) => {process.exit(r.statusCode === 200 ? 0 : 1)})"
 
 ENTRYPOINT ["dumb-init", "--"]
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 const app = require('./app');
 const config = require('./config');
 
-app.listen(config.port, () => {
+app.listen(config.port, "0.0.0.0",() => {
   console.log(`OpenVPN Control Plane API running on port ${config.port}`);
-  console.log(`JWT_SECRET: ${config.jwt.secret}`);
-  console.log(`JWT_EXPIRATION: ${config.jwt.expiration}`);
-  console.log(`Keep these tokens secure`);
-  console.log(`API Documentation: http://localhost:${config.port}/docs`);
+  if (process.env.NODE_ENV === "development") {
+    console.log(`JWT_SECRET: ${config.jwt.secret}`);
+    console.log(`JWT_EXPIRATION: ${config.jwt.expiration}`);
+    console.log(`Keep these tokens secure`);
+  }
+  console.log(`API Documentation: :${config.port}/docs`);
 });
